0d68f31db2b9ecc5eef739220b6a9620faf3892f
Gabe <ghoule636@gmail.com>
2022-09-26 11:14:39 -0700

    Adding some comments and cleaning up stuff.

--- drivers/screen.c
+++ drivers/screen.c
@@ -10,8 +10,8 @@ int get_offset_row(int offset);
 int get_offset_col(int offset);
 
 /**
- * @brief Prints a given string at the given location. If col or row are negative will print at 
- * cursor position.
+ * @brief Prints a given string at the given location. If col or row are
+ * negative will print at cursor position.
  * 
  * @param message String being printed must terminate in a null char.
  * @param col col of print location
@@ -29,14 +29,16 @@ void print_at(char *message, int col, int row) {
 
     int i = 0;
     while(message[i] != 0) {
-        offset = print_char(message[i++], col, row, WHITE_ON_BLACK);
+        offset = print_char(message[i], col, row, WHITE_ON_BLACK);
         row = get_offset_row(offset);
         col = get_offset_column(offset);
+        i++;
     }
 }
 
 /**
- * @brief Simple function that calls print_at and prints at current cursor location.
+ * @brief Simple function that calls print_at and prints at current cursor
+ * location.
  * 
  * @param message String to be printed.
  */
@@ -45,19 +47,21 @@ void print(char *message) {
 }
 
 /**
- * @brief prints a single char at the given col and row. If -1 is passed for col and row then cursor 
- * will be used as the location to print. Function updates cursor position and returns the offset of the
- * next char location.
+ * @brief prints a single char at the given col and row. If -1 is passed for col
+ * and row then cursor will be used as the location to print. Function updates
+ * cursor position and returns the offset of the next char location.
  * 
  * @param c char to be printed
  * @param col column of the char to be printed
  * @param row row of the char to be printed
- * @param attr style of char to be printed ex. 0x0f = white text on black background
+ * @param attr style of char to be printed 0x0f = white text on black background
  * @return int - the next char location
  */
 int print_char(char c, int col, int row, char attr) {
     unsigned char *vidmem = (unsigned char*) VIDEO_ADDRESS;
-    if (!attr) attr = WHITE_ON_BLACK;
+    if (!attr) {
+        attr = WHITE_ON_BLACK;
+    }
 
     if (col >= MAX_COLUMNS || row >= MAX_ROWS) {
         vidmem[2 * MAX_COLUMNS * MAX_ROWS - 2] = 'E';
@@ -66,8 +70,12 @@ int print_char(char c, int col, int row, char attr) {
     }
 
     int offset;
-    if (col >= 0 && row >= 0) offset = get_offset(col, row);
-    else offset = get_cursor();
+    if (col >= 0 && row >= 0) {
+        offset = get_offset(col, row);
+    } 
+    else {
+        offset = get_cursor();
+    } 
 
     if (c == '\n') {
         row = get_offset_row(offset);
@@ -82,14 +90,15 @@ int print_char(char c, int col, int row, char attr) {
 }
 
 int get_cursor() {
-    /* Screen cursor position: ask VGA control register (0x3d4) for bytes
-    * 14 = high byte of cursor and 15 = low byte of cursor. */
-    port_byte_out(REG_SCREEN_CTRL, 14); /* Requesting byte 14: high byte of cursor pos */
-    /* Data is returned in VGA data register (0x3d5) */
+    // Screen cursor position: ask VGA control register (0x3d4) for bytes
+    // 14 = high byte of cursor and 15 = low byte of cursor. */
+    // Requesting byte 14: high byte of cursor pos */
+    port_byte_out(REG_SCREEN_CTRL, 14); 
+    // Data is returned in VGA data register (0x3d5)
     int position = port_byte_in(REG_SCREEN_DATA);
     position = position << 8; /* high byte */
 
-    port_byte_out(REG_SCREEN_CTRL, 15); /* requesting low byte */
+    port_byte_out(REG_SCREEN_CTRL, 15); // requesting low byte
     position += port_byte_in(REG_SCREEN_DATA);
     // VGA 'cells' consist of the character and its control data
     // so this means we have to multiply the position by two so we offset
@@ -120,13 +129,16 @@ void clear_screen() {
 int get_offset(int col, int row) {
     return 2 * (row * MAX_COLUMNS + col);
 }
+
 int get_offset_row(int offset) {
     return offset / (2 * MAX_COLUMNS);
 }
+
 /*
- * Calculates the column by getting the row then translating that into the offset of start of the
- * row by multiplying by MAX_COLUMNS and by 2 then subtracting the offset which gives us our column 
- * and then we divide by 2 to compensate for the VGA char format.
+ * Calculates the column by getting the row then translating that into the
+ * offset of start of the row by multiplying by MAX_COLUMNS and by 2 then
+ * subtracting the offset which gives us our column and then we divide by 2 to
+ * compensate for the VGA char format.
  */
 int get_offset_column(int offset) {
     return (offset - (get_offset_row(offset) * 2 * MAX_COLUMNS)) / 2;
