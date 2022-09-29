e47bf0846636c4adc24c7d5adb0b61483e0390b7
Gabe <ghoule636@gmail.com>
2022-09-28 22:22:17 -0700

    Adding integer to ascii conversion function

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -5,14 +5,25 @@ void main() {
     clear_screen();
     // print("Hello World!\n");
 
-    for (int i = 0; i < MAX_COLUMNS * MAX_ROWS - 1; i++){
-        print("1");
-    }
+    // for (int i = 0; i < MAX_COLUMNS * MAX_ROWS - 1; i++){
+    //     print("1");
+    // }
     
-    for (int i = 0; i < MAX_COLUMNS; i++) {
-        print("2");
+    // for (int i = 0; i < MAX_COLUMNS; i++) {
+    //     print("2");
+    // }
+    // for (int i = 0; i < MAX_COLUMNS; i++) {
+    //     print("3");
+    // }
+
+    /* Fill up the screen */
+    int i = 0;
+    for (i = 0; i < 24; i++) {
+        char str[255];
+        int_to_ascii(i, str);
+        print_at(str, 0, i);
     }
-    for (int i = 0; i < MAX_COLUMNS; i++) {
-        print("3");
-    }
+
+    print_at("This text forces the kernel to scroll. Row 0 will disappear. ", 60, 24);
+    print("And with this text, the kernel will scroll again, and row 1 will disappear too!");
 }
